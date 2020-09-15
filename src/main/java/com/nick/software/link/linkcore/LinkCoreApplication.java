0cb0ab2cf1edb93150a2ec32eef1866f9dbde52e
NickThe1 <nikolay.kotlyarov1@gmail.com>
2020-09-15 12:48:24 +0600

    first commint

---
+++ src/main/java/com/nick/software/link/linkcore/LinkCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nick.software.link.linkcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkCoreApplication.class, args);
+	}
+
+}
