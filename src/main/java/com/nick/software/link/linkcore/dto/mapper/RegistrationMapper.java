8dc2618a4aa5ae8fba236657d9744d52d7d03a02
NickThe1 <nikolay.kotlyarov1@gmail.com>
2020-09-15 20:17:16 +0600

    .

--- src/main/java/com/nick/software/link/linkcore/dto/mapper/RegistrationMapper.java
+++ src/main/java/com/nick/software/link/linkcore/dto/mapper/RegistrationMapper.java
@@ -1,4 +1,15 @@
 package com.nick.software.link.linkcore.dto.mapper;
 
-public class RegistrationMapper {
+import com.nick.software.link.linkcore.dto.RegistrationDTO;
+import com.nick.software.link.linkcore.model.Account;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RegistrationMapper {
+    RegistrationMapper INSTANCE = Mappers.getMapper(RegistrationMapper.class);
+
+    Account registrationDTOToAccount(RegistrationDTO registrationDTO);
+
+    RegistrationDTO accountToRegistrationDTO(Account account);
 }
---
+++ src/main/java/com/nick/software/link/linkcore/service/AccountServiceImp.java
@@ -0,0 +1,30 @@
+package com.nick.software.link.linkcore.service;
+
+import com.nick.software.link.linkcore.dto.RegistrationDTO;
+import com.nick.software.link.linkcore.dto.mapper.RegistrationMapper;
+import com.nick.software.link.linkcore.model.Account;
+import com.nick.software.link.linkcore.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImp {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountServiceImp(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts(){
+        return accountRepository.findAll();
+    }
+
+    public void registerAccount(RegistrationDTO registrationDTO){
+        Account account = RegistrationMapper.INSTANCE.registrationDTOToAccount(registrationDTO);
+        accountRepository.save(account);
+    }
+}
